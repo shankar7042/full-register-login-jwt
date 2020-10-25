6771acd27b1adeae90dedc096c38e0c290f5ea78
shankar7042 <shankar.kumar.4171@gmail.com>
2020-10-25 10:22:27 +0530

    mongoDB database connection

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use("/", express.static(path.join(__dirname, "static")));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+mongoose.connect(
+  "mongodb://localhost/full-register-login",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("MongoDB connected");
+    }
+  }
+);
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.json({
+    status: "ok",
+    data: req.body,
+  });
 });
 
 app.listen(4000, () => {
